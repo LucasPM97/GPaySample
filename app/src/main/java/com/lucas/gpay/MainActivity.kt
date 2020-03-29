e3d26e2ae35094bad7b51b6d1f07f8fa1955c7ff
Lucas <lucasmontanari.97@gmail.com>
2020-03-29 19:57:19 -0300

    Add Nav Toolbar

--- app/src/main/java/com/lucas/gpay/MainActivity.kt
+++ app/src/main/java/com/lucas/gpay/MainActivity.kt
@@ -2,17 +2,25 @@ package com.lucas.gpay
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lucas.gpay.ui.nav.NavHostFragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController:NavController;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, NavHostFragment.newInstance())
-                    .commitNow()
-        }
+
+        navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/lucas/gpay/ui/nav/NavHostViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.lucas.gpay.ui.nav
-
-import androidx.lifecycle.ViewModel
-
-class NavHostViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
