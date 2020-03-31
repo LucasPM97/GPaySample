edaddb1149f81faefda2ef1deacc99be8bffdb46
Lucas <lucasmontanari.97@gmail.com>
2020-03-31 17:02:56 -0300

    Add MutableLiveData to change the price

--- app/src/main/java/com/lucas/gpay/ui/pay/PayFragment.kt
+++ app/src/main/java/com/lucas/gpay/ui/pay/PayFragment.kt
@@ -5,16 +5,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.RadioGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.lucas.gpay.R
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.fragment_pay.*
-import kotlinx.android.synthetic.main.main_fragment.*
 import kotlinx.android.synthetic.main.main_fragment.avatar_image
 
 class PayFragment : Fragment() {
@@ -33,8 +33,24 @@ class PayFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(PayViewModel::class.java)
 
-        Picasso.get().load(viewModel.avatarUrl).transform(CropCircleTransformation()).into(avatar_image);
+        Picasso.get().load(viewModel.getAvatarUrl()).transform(CropCircleTransformation()).into(avatar_image);
 
+        val price:MutableLiveData<Int?>? = viewModel.getMountToPay()
+
+        price?.observe(viewLifecycleOwner, Observer {
+            priceMount_text.text = "$ ${it.toString()}"
+        })
+
+        optionList.setOnCheckedChangeListener{ group, checkedId ->
+                val newPrice =  when(checkedId){
+                    price1.id-> 1
+                    price2.id-> 2
+                    price3.id-> 5
+                    price4.id-> 10
+                    else -> 0
+                }
+            viewModel.setMountToPay(newPrice)
+        }
     }
 
     override fun onAttach(context: Context) {
--- app/src/main/java/com/lucas/gpay/ui/pay/PayViewModel.kt
+++ app/src/main/java/com/lucas/gpay/ui/pay/PayViewModel.kt
@@ -1,12 +1,29 @@
 package com.lucas.gpay.ui.pay
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PayViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 
-    var avatarUrl: String? = "https://github.com/LucasPM97.png"
+    private var avatarUrl: String? = "https://github.com/LucasPM97.png"
 
-    var mountToPay:Int = 0
+    fun getAvatarUrl():String?{
+        return avatarUrl
+    }
+
+    private var mountToPay:MutableLiveData<Int?>? = null
+
+    fun getMountToPay():MutableLiveData<Int?>? {
+        if (mountToPay == null){
+            mountToPay = MutableLiveData<Int?>()
+            setMountToPay(0)
+        }
+        return mountToPay
+    }
+
+    fun setMountToPay(price:Int){
+        mountToPay?.value = price
+    }
 
 }
